cbec7f93afc42b5c5c896171fc730bf04ea63fcf
CJ Park <cpar831@aucklanduni.ac.nz>
2016-03-19 17:58:30 +1300

    bug fixed + checking for repeat obfuscations. Only thing left to do is to reiterate through all the files and check once more.

--- Obfuscator/src/obfuscate/ClassNameObfuscator.java
+++ Obfuscator/src/obfuscate/ClassNameObfuscator.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,11 +18,12 @@ public class ClassNameObfuscator implements Obfuscater{
 	@Override
 	public HashMap<String, File> execute(HashMap<String, File> files) throws IOException {
 		List<String> linesOfCode = new ArrayList<String>();
-
+		// List containing obfuscated names - So we don't obfuscated class names that have
+		// already been obfuscated
+		List<String> obfuscatedNames = new ArrayList<String>();
 		// Hashmap containing the old name as key and new name as value
 		HashMap<String, String> classNames = new HashMap<String,String>();
-		// List of all names of classes encountered so far
-		List<String> classNameList = new ArrayList<String>();
+
 
 		for (Map.Entry<String, File> fileEntry : files.entrySet()) {
 			File file = fileEntry.getValue();
@@ -36,8 +38,9 @@ public class ClassNameObfuscator implements Obfuscater{
 				// get the class name in the line. If there is no class name, the 
 				// string should be null
 				String className = classNameReturner(lineInFile);
-				// if it isn't null, iterate through hashmap to check if name exists
-				if (className != null) {
+				// if it isn't null and if the name hasn't been obfuscated before
+				// iterate through hashmap to check if name exists
+				if ((className != null) && (obfuscatedChecker(obfuscatedNames, className) == false)) {
 					for (String key : classNames.keySet()) {
 						// if the name already exists, check the value of the key 
 						// which is the new name of the class
@@ -52,25 +55,25 @@ public class ClassNameObfuscator implements Obfuscater{
 					// and add the class name to the list of encountered classes
 					// and put it in the hashmap and also rename it in the line of code
 					if (existsInHash == false) {
-						classNameList.add(className);
 						String obfName = asciiToString(name_counter);
+						obfuscatedNames.add(obfName);
 						classNames.put(className, obfName);
 						lineInFile = renameClass(obfName, className, lineInFile);
 					}
 				}
 				// Iterate through all class names encountered and replace in line of code if
 				// necessary
-				for (String s: classNameList) {
-					lineInFile.replaceAll(s, classNames.get(s));
+				for (String key: classNames.keySet()) {
+					lineInFile = lineInFile.replaceAll(key, classNames.get(key));
 				}
 				// Add the line of code to the list of lines
 				linesOfCode.add(lineInFile);
 			}
-			
-			
+
+
 			FileWriter fileWriter = new FileWriter(file);
 			BufferedWriter fileOutput = new BufferedWriter(fileWriter);
-			
+
 			// Write out the list
 			for (String s: linesOfCode) {
 				s = s + "\n";
@@ -82,7 +85,7 @@ public class ClassNameObfuscator implements Obfuscater{
 		}
 		return files;
 	}
-	
+
 	/*
 	 * Method that rewrites a line of code with a class name in it
 	 */
@@ -91,7 +94,18 @@ public class ClassNameObfuscator implements Obfuscater{
 		return result;
 	}
 
-	
+	/*
+	 * Method that checks whether a class has been obfuscated already or not.
+	 */
+	private boolean obfuscatedChecker(List<String> list, String className) {
+		for (String s: list) {
+			if (s.equals(className)){
+				return true;
+			}
+		}
+		return false;
+	}
+
 	/*
 	 * Method that should check if there is a class declaration or interface declaration
 	 * and returns the name of it. Returns null if the line of code does not have either of these
