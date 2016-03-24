245904b9c9c7ccc4fa5acaeeb10832218d5f1825
unknown <jkim506@aucklanduni.ac.nz>
2016-03-25 11:26:29 +1300

    Added check to ask for location services permission

--- TryReverseEngineerThis/app/src/main/java/com/woop/tryreverseengineerthis/LandingActivity.java
+++ TryReverseEngineerThis/app/src/main/java/com/woop/tryreverseengineerthis/LandingActivity.java
@@ -2,6 +2,7 @@ package com.woop.tryreverseengineerthis;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
@@ -9,10 +10,12 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -65,6 +68,41 @@ public class LandingActivity extends AppCompatActivity
 
         CurrentLocationListener locationListener = new CurrentLocationListener();
 
+        boolean gps_enabled = false;
+        boolean network_enabled = false;
+
+        if(locationManager==null)
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        try{
+            gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        }catch(Exception ex){}
+        try{
+            network_enabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        }catch(Exception ex){}
+
+        if(!gps_enabled && !network_enabled){
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            dialog.setMessage("Please turn on Location Service for the full experience" );
+            dialog.setPositiveButton("Setting", new DialogInterface.OnClickListener() {
+
+
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    // TODO Auto-generated method stub
+                    startActivityForResult(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS), 100);
+                }
+            });
+            dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    // TODO Auto-generated method stub
+
+                }
+            });
+            dialog.show();
+
+        }
         try {
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
                     0, 0, locationListener);
