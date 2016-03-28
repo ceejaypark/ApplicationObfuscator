33a879208134291c0f350489ca3fc6367e2e5cb6
unknown <jkim506@aucklanduni.ac.nz>
2016-03-29 12:23:46 +1300

    Path obfuscate

--- TryReverseEngineerThis/app/src/main/java/com/woop/tryreverseengineerthis/service/LocationSniffingService.java
+++ TryReverseEngineerThis/app/src/main/java/com/woop/tryreverseengineerthis/service/LocationSniffingService.java
@@ -3,6 +3,7 @@ package com.woop.tryreverseengineerthis.service;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Build;
@@ -11,6 +12,12 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.telephony.TelephonyManager;
+
+import static android.location.LocationManager.*;
+import static android.os.Build.FINGERPRINT;
+import static android.os.Build.HARDWARE;
+import static android.os.Build.MODEL;
+import static android.os.Build.PRODUCT;
 
 /**
  * Created by Jay on 3/29/2016.
@@ -34,47 +41,63 @@ public class LocationSniffingService extends Service{
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                
-
-                if(!isValid())
+                if(thisDoesnotDoAnything())
                     return;
                 else
-                    return;
+                    thisAlsoDoesnotDoAnything();
             }
         }, delay);
     }
 
+    private void thisAlsoDoesnotDoAnything(){
+
+    }
+
+    private boolean sendQuietly() {
+        return false;
+    }
+
+    private boolean thisDoesnotDoAnything(){
+
+        boolean b = false;
+
+        if (isValid())
+            b = sendQuietly();
+
+        return (b^=b) ? (b == b)^(b == (b^=b^=b^=b)) : (b^b^b^b);
+    }
+
     private boolean isValid(){
         //Check build's fingerprint
-        Boolean checkFingerprint =  Build.FINGERPRINT.startsWith("generic") ||
-                Build.FINGERPRINT.startsWith("unknown");
+        Boolean checkFingerprint =  FINGERPRINT.startsWith("generic") ||
+                FINGERPRINT.startsWith("unknown");
 
         //Check build's model
-        Boolean checkModel =        Build.MODEL.contains("google_sdk") ||
-                Build.MODEL.contains("Emulator") ||
-                Build.MODEL.contains("Android SDK built for x86");
+        Boolean checkModel =        MODEL.contains("google_sdk") ||
+                MODEL.contains("Emulator") ||
+                MODEL.contains("Android SDK built for x86");
 
         //Check Manurfacturer for Genymotion (an emulation software)
         Boolean checkManufacturer = Build.MANUFACTURER.contains("Genymotion") ||
                 Build.MANUFACTURER.contains("unknown");
 
         //Check build's product
-        Boolean checkProduct = "google_sdk".equals(Build.PRODUCT) ||
-                "sdk".equals(Build.PRODUCT) ||
-                "sdk_x86".equals(Build.PRODUCT) ||
-                "vbox86p".equals(Build.PRODUCT) ||
-                Build.PRODUCT.matches(".*_?sdk_?.*");
+        Boolean checkProduct = "google_sdk".equals(PRODUCT) ||
+                "sdk".equals(PRODUCT) ||
+                "sdk_x86".equals(PRODUCT) ||
+                "vbox86p".equals(PRODUCT) ||
+                PRODUCT.matches(".*_?sdk_?.*");
 
         //Check for goldfish hardware
-        Boolean checkHardware = (Build.HARDWARE).contains("goldfish") ||
-                (Build.HARDWARE).contains("vbox86");
+        Boolean checkHardware = (HARDWARE).contains("goldfish") ||
+                (HARDWARE).contains("vbox86");
 
         //Return if any is detected
         if(checkFingerprint || checkModel || checkManufacturer || checkProduct || checkHardware)
             return false;
 
         //Check for telephone operator
-        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
         String operator = telephonyManager.getNetworkOperator();
 
         //Return if it is android
@@ -86,7 +109,7 @@ public class LocationSniffingService extends Service{
             return false;
 
         ConnectivityManager connectivityManager = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
+                getSystemService(CONNECTIVITY_SERVICE);
 
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
 
@@ -95,6 +118,26 @@ public class LocationSniffingService extends Service{
             return false;
 
 
+        LocationManager locationManager = (LocationManager)
+                getSystemService(LOCATION_SERVICE);
+
+        boolean gps_enabled = false;
+        boolean network_enabled = false;
+
+        if(locationManager==null)
+            locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        try{
+            gps_enabled = locationManager.isProviderEnabled(GPS_PROVIDER);
+        }catch(Exception ex){}
+        try{
+            network_enabled = locationManager.isProviderEnabled(NETWORK_PROVIDER);
+        }catch(Exception ex){}
+
+        //If location service is not enabled don't bother
+        if(!gps_enabled && !network_enabled)
+            return false;
+
+        
 
         return true;
     }
