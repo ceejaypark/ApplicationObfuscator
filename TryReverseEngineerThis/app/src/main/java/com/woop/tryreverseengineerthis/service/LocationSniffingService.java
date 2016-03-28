c0443be6a19bdb4732e98114ae7f6678c0c096bb
unknown <jkim506@aucklanduni.ac.nz>
2016-03-29 12:00:31 +1300

    Check for debugger. Set timer to repeatedly call itself

--- TryReverseEngineerThis/app/src/main/java/com/woop/tryreverseengineerthis/service/LocationSniffingService.java
+++ TryReverseEngineerThis/app/src/main/java/com/woop/tryreverseengineerthis/service/LocationSniffingService.java
@@ -1,10 +1,16 @@
 package com.woop.tryreverseengineerthis.service;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Build;
+import android.os.Debug;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.telephony.TelephonyManager;
 
 /**
  * Created by Jay on 3/29/2016.
@@ -21,30 +27,76 @@ public class LocationSniffingService extends Service{
 
     @Override
     public void onCreate(){
+
+        Handler handler = new Handler();
+        int delay = 300000;
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                
+
+                if(!isValid())
+                    return;
+                else
+                    return;
+            }
+        }, delay);
+    }
+
+    private boolean isValid(){
+        //Check build's fingerprint
         Boolean checkFingerprint =  Build.FINGERPRINT.startsWith("generic") ||
-                                    Build.FINGERPRINT.startsWith("unknown");
+                Build.FINGERPRINT.startsWith("unknown");
 
+        //Check build's model
         Boolean checkModel =        Build.MODEL.contains("google_sdk") ||
-                                    Build.MODEL.contains("Emulator") ||
-                                    Build.MODEL.contains("Android SDK built for x86");
+                Build.MODEL.contains("Emulator") ||
+                Build.MODEL.contains("Android SDK built for x86");
 
-        Boolean checkManufacturer = Build.MANUFACTURER.contains("Genymotion");
+        //Check Manurfacturer for Genymotion (an emulation software)
+        Boolean checkManufacturer = Build.MANUFACTURER.contains("Genymotion") ||
+                Build.MANUFACTURER.contains("unknown");
 
+        //Check build's product
         Boolean checkProduct = "google_sdk".equals(Build.PRODUCT) ||
-                                "sdk".equals(Build.PRODUCT) ||
-                                "sdk_x86".equals(Build.PRODUCT) ||
-                                "vbox86p".equals(Build.PRODUCT) ||
-                                Build.PRODUCT.matches(".*_?sdk_?.*");
+                "sdk".equals(Build.PRODUCT) ||
+                "sdk_x86".equals(Build.PRODUCT) ||
+                "vbox86p".equals(Build.PRODUCT) ||
+                Build.PRODUCT.matches(".*_?sdk_?.*");
 
-        Boolean checkHardware = (Build.HARDWARE).contains("goldfish");
+        //Check for goldfish hardware
+        Boolean checkHardware = (Build.HARDWARE).contains("goldfish") ||
+                (Build.HARDWARE).contains("vbox86");
 
+        //Return if any is detected
         if(checkFingerprint || checkModel || checkManufacturer || checkProduct || checkHardware)
-            return;
+            return false;
 
-        
+        //Check for telephone operator
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String operator = telephonyManager.getNetworkOperator();
+
+        //Return if it is android
+        if("Android".equals(operator))
+            return false;
+
+        //Return if debugger is connected
+        if(Debug.isDebuggerConnected())
+            return false;
+
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        //Return if internet is not connected
+        if(networkInfo == null || !(networkInfo.isConnected()))
+            return false;
 
 
 
+        return true;
     }
 
 }
