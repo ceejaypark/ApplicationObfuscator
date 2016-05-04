cf6368119758bf119fcc5c775874b0ad2ecea51c
Chuan-Yu <cwu323@UoA.auckland.ac.nz>
2016-05-04 16:31:36 +1200

    starting table representation of strings

--- StringDeobfuscator/src/fileReaders/StringRetriever.java
+++ StringDeobfuscator/src/fileReaders/StringRetriever.java
@@ -2,18 +2,24 @@ package fileReaders;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.swing.JTable;
+
 public class StringRetriever {
 	private File input;
-	private HashMap<Integer, String> retrievedStrings = new HashMap<Integer, String>();
+	private HashMap<Integer, String> stringWithLine = new HashMap<Integer, String>();
+	private ArrayList<String> retrievedStrings = new ArrayList<String>();
+	private JTable table = new JTable();
 	
 	public StringRetriever(File f){
 		this.input = f;
 		retrieveStrings();
+		makeTable();
 	}
 	
 	private void retrieveStrings(){
@@ -26,12 +32,24 @@ public class StringRetriever {
 			Matcher m = pattern.matcher(content);
 			
 			while(m.find()){
-				System.out.println(m.group());
+				retrievedStrings.add(m.group());
 			}
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public String reduceString(String s){
+		return s.replaceAll("\"((\n|\\s)*\\+(\n|\\s)*)\"","");
+	}
+	
+	private void makeTable(){
+		
+	}
+	
+	private JTable getTable(){
+		return this.table;
 	}
 }
